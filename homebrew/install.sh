8ed3336e8ced3c708287c54e7bafd5a65008c78a
Brian Jenkins <brian@jenkinz.com>
2024-07-13 10:09:49 -0700

    Use ruby to execute script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -11,13 +11,7 @@ then
   echo "  Installing Homebrew for you."
 
   # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/HEAD/install.sh)"
-  fi
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 fi
 
